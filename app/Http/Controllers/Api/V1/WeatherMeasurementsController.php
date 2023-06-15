3d5ceb222fc32545673f7102832051912f43fb9b
Jurrie Piek <jurrie39@gmail.com>
2023-06-15 12:56:33 +0200

    added distance from station to given coordinates

--- app/Http/Controllers/Api/V1/WeatherMeasurementsController.php
+++ app/Http/Controllers/Api/V1/WeatherMeasurementsController.php
@@ -34,6 +34,33 @@ class WeatherMeasurementsController
                 ],
             ],
             '$maxDistance' => $request->get('distance') * 1000,
-        ])->select('name')->get();
+        ])->select('name', 'location')->get()->each(function ($station) use ($request) {
+            $station->distance = $this->calculateDistance(
+                $request->get('latitude'),
+                $request->get('longitude'),
+                $station['location']['coordinates'][1],
+                $station['location']['coordinates'][0]
+            );
+            unset($station['location']);
+        });
+    }
+
+    //Magic van het internet, but it works
+    private function calculateDistance(
+        $latitudeFrom, $longitudeFrom, $latitudeTo, $longitudeTo, $earthRadius = 6371)
+    {
+        // convert from degrees to radians
+        $latFrom = deg2rad($latitudeFrom);
+        $lonFrom = deg2rad($longitudeFrom);
+        $latTo = deg2rad($latitudeTo);
+        $lonTo = deg2rad($longitudeTo);
+
+        $lonDelta = $lonTo - $lonFrom;
+        $a = pow(cos($latTo) * sin($lonDelta), 2) +
+            pow(cos($latFrom) * sin($latTo) - sin($latFrom) * cos($latTo) * cos($lonDelta), 2);
+        $b = sin($latFrom) * sin($latTo) + cos($latFrom) * cos($latTo) * cos($lonDelta);
+
+        $angle = atan2(sqrt($a), $b);
+        return number_format($angle * $earthRadius, 2);
     }
 }
